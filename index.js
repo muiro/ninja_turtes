2efd89796753e5d827534132ac8d4e8ec87c6c72
muiro <wubaga@gmail.com>
2015-04-11 23:42:14 -0400

    POC
    Started adding testing framework and a POC for turtle messages.

---
+++ index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var express = require('express');
+var app = express();
+var log4js = require('log4js');
+var logger = log4js.getLogger();
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+
+app.post('/api/turtle_message', jsonParser, function(req, res){
+	logger.debug('Got a turtle message!');
+	logger.debug(req.body);
+});
+
+var server = app.listen(3000, function(){
+	var host = server.address().address;
+	var port = server.address().port;
+
+	logger.debug('ninja_turtles server listening at http://%s:%s', host, port);
+});
---
+++ test/test.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var assert = require("assert");
+
+describe ("ninja_turtles", function(){
+	describe("turtle_message", function(){
+		it("should accept a json message", function(){
+			
+		});
+	});
+});
