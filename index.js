d858f47297f4266d35170ce779c51d102fbd6b40
muiro <wubaga@gmail.com>
2015-04-13 14:06:10 -0400

    Added message queue management functions and parts of the message api

--- index.js
+++ index.js
@@ -3,22 +3,41 @@
 var express = require('express');
 var app = express();
 var log4js = require('log4js');
-var logger = null;
+var logger = logger = log4js.getLogger();
 var bodyParser = require('body-parser');
+var UUID = require('node-uuid');
 
-if (process.env.NODE_ENV !== 'test') {
-	log4js.loadAppender('console');
-	log4js.addAppender(log4js.appenders.console(), 'logger');
-	logger = log4js.getLogger('logger');
-} 
+if (process.env.NODE_ENV != 'test') {
+	logger.setLevel('DEBUG');
+} else {
+	logger.setLevel('ERROR');
+}
+
+var messages = [];
+var message_limit = 100;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/api/turtle_message', function(req, res){
-	logger.debug('Got a turtle message!');
+app.post('/api/message', function(req, res){
+	logger.debug('Got a message!');
 	var message = fix_cc_message(req.body);
-	res.send({status: "ok"});
+	if (message != null) {
+		log_message(message);
+		res.send({status: "ok"});
+	} else {
+		res.send({status: "error"});
+	}
+});
+
+app.get('/api/message/:id?', function(req, res){
+	logger.debug('Received a message get request!');
+	var this_message = get_message(req.params.id);
+	if (this_message != null) {
+		res.send(this_message);
+	} else {
+		res.send({status: 'error'});
+	}
 });
 
 app.get('/test', function(req, res){
@@ -35,10 +54,62 @@ var server = app.listen(3000, function(){
 
 function fix_cc_message(message) {
 	var data = null;
-	for(var key in message) {
-		data = JSON.parse(key);
+	try {
+		for(var key in message) {
+			data = JSON.parse(key);
+		}
+
+		return data;
+	} catch (error) {
+		return null;
 	}
-	return data;
+}
+
+function log_message(message) {
+	messages.push(message);
+	while (messages.length > message_limit) {
+		messages.shift();
+	}
+	return true;
+}
+
+function purge_messages() {
+	messages = [];
+	return true;
+}
+
+function get_messages() {
+	try {
+		return messages.slice(0);
+	} catch (error) {
+		logger.error(error);
+		return null;
+	}
+}
+
+function get_message(uuid) {
+	try {
+		var this_messages = get_messages();
+		if (uuid == null || uuid == undefined) {
+			return this_messages[this_messages.length -1];
+		} else {
+			if (this_messages.map(function(element){return element.uuid;}).indexOf(uuid) != -1) {
+				return this_messages[this_messages.map(function(element){return element.uuid;}).indexOf(uuid)];
+			} else {
+				logger.debug(uuid + " not in message queue");
+				return null;
+			}
+		}
+		return null;
+	} catch (error) {
+		logger.error(error);
+		return null;
+	}
 }
 
 exports.fix_cc_message = fix_cc_message;
+exports.log_message = log_message;
+exports.get_messages = get_messages;
+exports.purge_messages = purge_messages;
+exports.message_limit = message_limit;
+exports.get_message = get_message;
--- test/test.js
+++ test/test.js
@@ -3,11 +3,8 @@
 var expect = require('chai').expect;
 var request = require('supertest');
 process.env.NODE_ENV = 'test';
+var UUID = require('node-uuid');
 var app = require(__dirname + '/../index.js');
-var log4js_debug = require('log4js');
-
-var logger = log4js_debug.getLogger();
-
 
 describe("ninja_turtles", function(){
 
@@ -20,34 +17,209 @@ describe("ninja_turtles", function(){
 			expect(JSON.stringify(message_object)).to.equal(JSON.stringify({message: 'test message'}));
 			done();
 		});
+
+		it("should return null if data is bad", function(done){
+			var message = {
+				'{"message":"test message"': ''
+			};
+			var message_object = app.fix_cc_message(message);
+			expect(message_object).to.equal(null);
+			done();
+		});
 	});
 
-	describe("turtle_message", function(){
-		it("should accept a json message and return ", function(done){
+	describe("#get_message()", function(){
+		it("should return a message by uuid", function(done){
+			app.purge_messages();
+			var uuid = UUID.v4();
 			var message = {
-				'{"message":"test message"}': ''
+				uuid: uuid,
+				message: "#get_message(uuid) test message"
 			};
-			request("http://localhost:3000")
-			.post("/api/turtle_message")
-			.send(message)
-			// .end(function(err, res){
-			// 	if (err) {
-			// 		throw err;
-			// 	}
-			// 	// for (var key in res) {
-			// 	// 	logger.debug(key);
-			// 	// }
-			// 	// logger.debug(res.request);
-			// 	var keys = Object.keys(res.request);
-			// 	keys.sort();
-			// 	for (var i = 0; i < keys.length; i++) {
-			// 		logger.debug(keys[i]);
-			// 	}
-			// 	done();
-			// });-
-			.end(function(err, res){
-				if (err) return done(err);
-				done();
+			app.log_message(message);
+			var result = app.get_message(uuid);
+			expect(result).to.deep.equal(message);
+			done();
+		});
+
+		it("should return null if no message by that uuid exists", function(done){
+			app.purge_messages();
+			var uuid = UUID.v4();
+			var result = app.get_message(uuid);
+			expect(result).to.equal(null);
+			done();
+		});
+
+		it("should return the most recent message if no uuid is passed", function(done){
+			app.purge_messages();
+			var uuid = UUID.v4();
+			var uuid2 = UUID.v4();
+			var message = {
+				uuid: uuid,
+				message: "#get_message(uuid) test message 1"
+			};
+			var message2 = {
+				uuid: uuid2,
+				message: "#get_message(uuid) test message 2"
+			};
+			app.log_message(message);
+			app.log_message(message2);
+			var result = app.get_message();
+			expect(result).to.deep.equal(message2);
+			done();
+		});
+	});
+
+	describe("#get_messages()", function(){
+		it("should return the message queue", function(done){
+			app.purge_messages();
+			var messages = app.get_messages();
+			expect(messages).to.deep.equal([]);
+			done();
+		});
+	});
+
+	describe("#log_message()", function(){
+		it("should accept a message and place it in the messages queue", function(done){
+			app.purge_messages();
+			app.log_message("test");
+			expect(app.get_messages()).to.deep.equal(['test']);
+			done();
+		});
+
+		it("should add a second message to the queue", function(done){
+			app.purge_messages();
+			app.log_message("test");
+			app.log_message("test2");
+			expect(app.get_messages()).to.deep.equal(['test', 'test2']);
+			done();
+		});
+
+		it("should only keep " + app.message_limit + " messages in the queue", function(done){
+			app.purge_messages();
+			var messages = [];
+			for (var i = 0; i < app.message_limit + 10; i++) {
+				app.log_message("test " + i);
+				messages.push("test " + i);
+			}
+			while (messages.length > app.message_limit) {
+				messages.shift();
+			}
+			expect(app.get_messages()).to.deep.equal(messages);
+			done();
+		});
+	});
+
+	describe("#purge_messages()", function(){
+		it("should set blank out the messages queue", function(done){
+			app.purge_messages();
+			app.log_message("test");
+			app.purge_messages();
+			expect(app.get_messages()).to.deep.equal([]);
+			done();
+		});
+	});
+
+	describe("/api", function(){
+		describe("/message POST", function(){
+			it("should accept a json message and return status: ok", function(done){
+				var message = {
+					'{"message":"test message"}': ''
+				};
+				request("http://localhost:3000")
+				.post("/api/message")
+				.send(message)
+				.end(function(err, res){
+					if (err) return done(err);
+					expect(res.body).to.deep.equal({status: 'ok'});
+					done();
+				});
+			});
+
+			it("should return status: error if message is not json or it can't be parsed", function(done){
+				var message = {
+					'{"message":"test message"': ''
+				};
+				request("http://localhost:3000")
+				.post("/api/message")
+				.send(message)
+				.end(function(err, res){
+					if (err) return done(err);
+					expect(res.body).to.deep.equal({status: 'error'});
+					done();
+				});
+			});
+
+			it("should add new messages to the message queue", function(done){
+				app.purge_messages();
+				var message = {
+					'{"message":"test message2"}': ''
+				};
+				request("http://localhost:3000")
+				.post("/api/message")
+				.send(message)
+				.end(function(err, res){
+					if (err) return done(err);
+					expect(app.get_messages()).to.deep.equal([{message: "test message2"}]);
+					done();
+				});
+			});
+		});
+
+		describe("/message/:id GET", function(){
+			it("should return a message by the specified uuid", function(done){
+				app.purge_messages();
+				var uuid = UUID.v4();
+				var message = {
+					uuid: uuid,
+					message: "/message/:id GET test message"
+				};
+				app.log_message(message);
+				request("http://localhost:3000")
+				.get("/api/message/" + uuid)
+				.send()
+				.end(function(err, res){
+					if (err) return done(err);
+					expect(res.body).to.deep.equal(message);
+					done();
+				});
+			});
+
+			it("should return an error if the message doesn't exist in the queue", function(done){
+				app.purge_messages();
+				var uuid = UUID.v4();
+				request("http://localhost:3000")
+				.get("/api/message/" + uuid)
+				.send()
+				.end(function(err, res){
+					if (err) return done(err);
+					expect(res.body).to.deep.equal({status: 'error'});
+					done();
+				});
+			});
+
+			it("should return the most recent message if no uuid is specified", function(done){
+				app.purge_messages();
+				var uuid = UUID.v4();
+				var message = {
+					uuid: uuid,
+					message: "/message/:id GET test message 1"
+				};
+				var uuid2 = UUID.v4();
+				var message2 = {
+					uuid: uuid2,
+					message: "/message/:id GET test message 2"
+				};
+				app.log_message(message);
+				app.log_message(message2);
+				request("http://localhost:3000")
+				.get("/api/message/")
+				.send()
+				.end(function(err, res){
+					if (err) return done(err);
+					expect(res.body).to.deep.equal(message2);
+					done();
+				});
 			});
 		});
 	});
