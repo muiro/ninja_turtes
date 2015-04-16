dab13f955f8470d3af829659a7dbbfd2c6058e52
muiro <wubaga@gmail.com>
2015-04-15 22:18:36 -0400

    Computer scripts and tests

--- scripts/message.lua
+++ scripts/message.lua
@@ -7,11 +7,16 @@ function send(message)
 	local game_time = os.time()
 	data.game_time = textutils.formatTime(game_time, false)
 	data.message = message
-	data.turtle_name = os.computerLabel()
-	data.turtle_id = os.computerID()
-	
-	local data_string = json.encode(data)
-	local response = http.post("http://localhost:3000/api/message_message", data_string)
+	data.computer_name = os.computerLabel()
+	data.computer_id = os.computerID()
 
-	print(response.readAll())
+	local data_string = json.encode(data)
+	local response = http.post("http://localhost:3000/api/message", data_string)
+
+	return response.readAll()
+end
+
+function time()
+	local response = http.get("http://localhost:3000/api/time")
+	return response.readAll()
 end
